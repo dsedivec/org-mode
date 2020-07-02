870c93e257ec0b6b86ac97b9d9bccdfbf3d69d53
Dale Sedivec <dale@codefu.org>
2020-07-02 17:06:56 -0500

    Inherit the EMACS environment variable during build
    Needed for when I bootstrap a new computer and Emacs isn't on PATH yet.

--- build-emacs-package.sh
+++ build-emacs-package.sh
@@ -5,5 +5,5 @@ set -euo pipefail
 export PATH=/opt/local/libexec/gnubin:$PATH
 export MAKEFILES=mk/server.mk
 cd "$(dirname "$0")"
-make elpaplus archive-contents
+make EMACS="${EMACS:-emacs}" elpaplus archive-contents
 ls -ld archive-contents org-plus-contrib*
