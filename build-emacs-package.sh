64bbd24943f0c9148a7cdc82b0cd975592a975a7
Dale Sedivec <dale@codefu.org>
2021-10-06 17:09:59 -0500

    Remove package.el build script
    Recent moves to the FSF infrastructure (I believe) seem to have removed
    bits for building package.el packages from this repository, so I think
    this script no longer serves any purpose.

--- build-emacs-package.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-export PATH=/opt/local/libexec/gnubin:$PATH
-export MAKEFILES=mk/server.mk
-cd "$(dirname "$0")"
-make EMACS="${EMACS:-emacs}" elpaplus archive-contents
-ls -ld archive-contents org-plus-contrib*
